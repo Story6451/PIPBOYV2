aa893182b84a8a7e388779dcaa0e481d9fe388b3
story6451 <garan.cw@gmail.com>
2023-07-24 12:14:58 +0100

    making changes to healthtab

--- src/HealthTab.cpp
+++ src/HealthTab.cpp
@@ -19,49 +19,126 @@ HealthTab::HealthTab(Adafruit_TFTLCD *ptft, ConfigData *pPipData):Tab(ptft, pPip
 
 void HealthTab::Setup()
 {
+    //resets variables
     step = 0;
+    avgRawVal = 0;
+    stdDeviation = 0;
+    time1 = 0;
+    time2 = 0;
+    firstbeat = true;
     rawVal = 0;
-    delta = 0;
+    deltaTime = 0;
     bpm = 0;
-    preBpm = 0;
-    bpmRepeats = 0;
+
     meanBpm = 0;
-    arrayPosition = 0;
-    peak = 0;
-    beat = false;
+    valuesArrayPosition = 0;
+    bpmArrayPosition = 0;
+
     t1 = 0;
+    t2 = 0;
     t3 = 0;
 
     for (int i = 0; i < BUFFER_LENGTH -1; i++)
     {
         bpms[i] = 0;
-        deltas[i] = 0;
     }
     
-    //need to determine the peak so...
-    
+    //fills buffer
+    /*
     for (int i = 0; i < BUFFER_LENGTH - 1; i++)
     {
         values[i] = analogRead(heartPin);
         delay(5);
     }
-    
+    */
     pTFT->drawBitmap(270, 100, StatBoy, 200, 200, pPIPDATA->ActiveColour);
 }
+
+bool arrayIsFull = false;
 
 void HealthTab::Loop()
 {
     rawVal = analogRead(heartPin);
-    
+
+    if (rawVal > 150)
+    {
+        values[valuesArrayPosition] = rawVal;
+        times[valuesArrayPosition] = millis();
+
+        if (valuesArrayPosition == BUFFER_LENGTH - 1)
+        {
+            arrayIsFull = true;
+            valuesArrayPosition = 0;
+        }
+        else
+        {
+            valuesArrayPosition++;
+        }
+
+        if (arrayIsFull == true)
+        {
+            int64_t sumRawVal = 0;
+            int64_t sumRawValSquared = 0;
+            for(byte i = 0; i < BUFFER_LENGTH - 1; i++)
+            {
+                sumRawVal += values[i];
+                sumRawValSquared += values[i] * values[i];
+            }
+            
+            avgRawVal = sumRawVal / BUFFER_LENGTH;
+            stdDeviation = sqrt(sumRawValSquared/BUFFER_LENGTH - (avgRawVal * avgRawVal));
+
+        }
+
+        if ((rawVal - avgRawVal) > 0)   //then we have a peak
+        {
+            if (firstbeat == true)
+            {
+                time1 = millis();
+            }
+            else
+            {
+                time2 = millis();
+            }
+            firstbeat = !firstbeat;
+
+            deltaTime = abs(time1 - time2);
+
+            bpm = 60000/deltaTime;
+            bpms[bpmArrayPosition] = bpm;
+            
+            if (bpmArrayPosition == BUFFER_LENGTH - 1)
+            {
+                bpmArrayPosition = 0;
+            }
+            else
+            {
+                bpmArrayPosition++;
+            }
+            
+            int32_t sumOfBpms = 0;
+            for(int i = 0; i < BUFFER_LENGTH; i++)
+            {
+                sumOfBpms += bpms[i];
+            }
+            meanBpm = sumOfBpms / BUFFER_LENGTH;
+        }
+    }
+
+    /*
     if (rawVal > 150)
     {
         values[arrayPosition] = rawVal; //add data to array
         times[arrayPosition] = millis();
 
-        if(arrayPosition == BUFFER_LENGTH - 1)        
+        if(arrayPosition == BUFFER_LENGTH - 1) 
+        {
             arrayPosition = 0;
+        }       
         else
+        {
             arrayPosition++;
+        }
 
         bool peak = pd.addValue(values[arrayPosition]);
         if (peak)
@@ -75,7 +152,9 @@ void HealthTab::Loop()
         }
 
         if ((delta < 2500) && (delta > 250))
+        {
             deltas[arrayPosition] = delta;
+        }
 
         int32_t deltaSum = 0;
         for (int i = 0; i < BUFFER_LENGTH -1; i++)
@@ -101,48 +180,23 @@ void HealthTab::Loop()
         meanBpm = 0;
         bpm = 0;
     }
+    */
     OutputThroughSerial();
     TFTOutput();
 
 }
 
 int32_t HealthTab::StandardDeviation(int32_t dataSet[], int32_t SIZE){}
-/*{
-    int32_t dataSetSquared[SIZE];
-    int32_t m = 0;
-    for (int i = 0; i < SIZE -1; i++)
-    {
-        dataSetSquared[i] = dataSet[i] * dataSet[i];
-    }
-    
-    //calculate mean
-    int32_t sum = 0;
-    for (int i = 0; i < SIZE - 1; i++)
-    {
-        sum = sum + dataSet[i];
-    }
-    m = sum/SIZE;
-
-    //calculate standard deviation
-    //sum of values squared
-    int32_t sumSquared = 0;
-    for (int i = 0; i < SIZE - 1; i++)
-    {
-        sumSquared = sumSquared + dataSetSquared[i];
-    }
-    //apply formula
-    double temp1 = sumSquared/SIZE;
-    double temp2 = m * m;
-    double variance = temp1 - temp2;
-
-    return sqrt(variance);
-}*/
 
 void HealthTab::OutputThroughSerial()
 {
     Serial.print(" Analog value: "); Serial.print(rawVal);
-    Serial.print(" delta: "); Serial.print(delta);
-    Serial.print(" Mean BPM: "); Serial.println(meanBpm);
+    Serial.print(" Mean value: "); Serial.print(avgRawVal);
+    Serial.print(" Delta raw values: "); Serial.print(rawVal - avgRawVal);
+    Serial.print(" std dev value: "); Serial.print(stdDeviation);
+    Serial.print(" delta: "); Serial.print(deltaTime);
+    Serial.print(" Mean BPM: "); Serial.print(meanBpm);
+    Serial.print(" BPM: "); Serial.println(bpm);
 }
 
 void HealthTab::TFTOutput()
@@ -155,9 +209,27 @@ void HealthTab::TFTOutput()
 
         //displays bpm
         pTFT->setCursor(20, 100);
-        pTFT->print("AVG BPM: "); pTFT->print(meanBpm); pTFT->print("  ");
+        pTFT->print("AVG BPM: "); 
+        if (meanBpm < 300)
+        {
+            pTFT->print(meanBpm); pTFT->print("  "); 
+        }
+        else
+        {
+            pTFT->print("error"); 
+        }
+        pTFT->print("  ");
         pTFT->setCursor(20, 140);
-        pTFT->print("Calulated BPM:"); pTFT->print(bpm); pTFT->print(" ");
+        pTFT->print("BPM:"); 
+        if (bpm < 300)
+        {
+            pTFT->print(bpm);  pTFT->print("  "); 
+        }
+        else
+        {
+            pTFT->print("error"); 
+        }
+        pTFT->print(" ");
         t1 = t2;
     }
 
@@ -173,15 +245,4 @@ void HealthTab::TFTOutput()
     {
         t3 = t2;
     }
-
-/*
-    pTFT->drawFastVLine(step, (320 - map(rawVal, 0, 1023, 0, 140)), map(rawVal, 0, 1023, 0, 140), pPIPDATA->ActiveColour);
-    step++;
-
-    if (step >= 240)
-    {
-        step = 0;
-        pTFT->fillRect(0, 180, 240, 140, BLACK);
-    }
-    */
 }
--- src/HealthTab.h
+++ src/HealthTab.h
@@ -3,27 +3,26 @@
 #include <Arduino.h>
 #include <Tab.h>
 
-class HealthTab : public Tab //Add oxygen concentration reading features
+class HealthTab : public Tab
 {
   private:
     static const int BUFFER_LENGTH = 100;
+    float avgRawVal;
+    double stdDeviation;
     int32_t rawVal;
+    int8_t valuesArrayPosition;
     int32_t values[BUFFER_LENGTH];
     int32_t times[BUFFER_LENGTH];
+    bool firstbeat;
     int64_t time1;
     int64_t time2;
-    int32_t delta;
+    int32_t deltaTime;
     int16_t bpm;
-    int16_t preBpm;
-    int16_t bpmRepeats;
     int16_t meanBpm;
-    int32_t deltas[BUFFER_LENGTH];
-    int16_t meanDelta;
-    int32_t validBpm[BUFFER_LENGTH];
+    int8_t bpmArrayPosition;
     int32_t bpms[BUFFER_LENGTH];
-    int8_t arrayPosition;
-    int32_t peak;
-    bool beat;
+
+    
     int64_t t1;
     int64_t t2;
     int64_t t3;
